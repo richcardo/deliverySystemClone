c6f211b3e6661d10cf542517e72694f7de44132c
Riccardo Cardia <cardiariccardo94@gmail.com>
2024-06-20 00:03:31 +0200

    modifiche

--- resources/views/statistic.blade.php
+++ resources/views/statistic.blade.php
@@ -2,12 +2,14 @@
     <div class="container-fluid">
         <div class="row justify-content-center mt-5">
             <div class="col-6">
-                <div class="card">
+                <div class="card p-3">
                     <h3>Distanze</h3>
-                    <p>Maggiore distanza percorsa : {{ $distance }} Km</p>
+                    <p>Maggiore distanza percorsa : {{ round($distance,1) }} Km</p>
                     <p>Rider con maggior distanza : {{ $rider }}</p>
                 </div>
-                <div class="card mt-3">
+            </div>
+            <div class="col-6">
+                <div class="card p-3">
                     <h3>Consegne</h3>
                     <p>Maggior numero di consegne : {{ $most_deliveries }}</p>
                     <p>Rider con maggior numero di consegne : {{ $rider_most_deliveries }}</p>
